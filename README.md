f67d4f2c22bd764e9065c0cbc9011c3555a656bb
Reese Wilson <reesew@tzmedical.com>
2016-01-13 11:55:11 -0800

    modded and compiled

---
+++ README.md
@@ -0,0 +1,40 @@
+# Minecraft Horse Stats
+
+This is a port of Wubbi's mod.
+
+Rather than building complicated redstone horse speedometers, you can install this mod. To see its effect, tame a horse, then open your inventory. The stats, including speed, health and jump strength, will be displayed as a percentage.
+
+This is a client mod, meaning you can use it to modify your local installation of Minecraft and still gain the benefits while playing on SMP (survival multiplayer) servers
+
+## Compatibility
+
+Tested with Minecraft 1.8.8 and 1.8.9
+
+## Installation
+
+Currently only manual installation is provided. I'd gladly accept pull requests to ease the installation process using forge or whatever. See the following links for detailed manual installation instructions
+
+[http://minecraft.gamepedia.com/Mods/Installing_mods#Installing_Mods_Manually](http://minecraft.gamepedia.com/Mods/Installing_mods#Installing_Mods_Manually)
+
+[http://www.6minecraft.net/how-to-install-mods-minecraft/](http://www.6minecraft.net/how-to-install-mods-minecraft/)
+
+windows tl;dr
+
+* copy %appdata%/Roaming/.minecraft/versions/1.8.9 to %appdata%/Roaming/.minecraft/versions/1.8.9-horse-stats
+* rename 1.8.9.jar and 1.8.9.json to 1.8.9-horse-stats.jar and 1.8.9-horse-stats.json 
+* change the `id` field in 1.8.9-horse-stats.json from `1.8.9` to `1.8.9-horse-stats`
+* `open archive` 1.8.9-horse-stats.jar with 7zip
+* delete META-INF folder
+* drag azb.class into archive, then close 7zip
+* create a new profile in minecraft using version 1.8.9-horse-stats
+* play minecraft using new profile
+
+## Development
+
+I used the [Mod Coder Pack] to modify the class.
+
+* Run decompile.bat
+* Modify src\minecraft\net\minecraft\client\gui\inventory\GuiScreenHorseInventory.java
+* Run recompile.bat
+* Run reobfuscate.bat
+* Use files in `reobf` directory to mod Minecraft
--- src/minecraft/net/minecraft/client/gui/inventory/GuiScreenHorseInventory.java
+++ src/minecraft/net/minecraft/client/gui/inventory/GuiScreenHorseInventory.java
@@ -1,6 +1,7 @@
 package net.minecraft.client.gui.inventory;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.passive.EntityHorse;
 import net.minecraft.inventory.ContainerHorseInventory;
@@ -41,7 +42,11 @@ public class GuiScreenHorseInventory extends GuiContainer
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
     {
         this.fontRendererObj.drawString(this.horseInventory.getDisplayName().getUnformattedText(), 8, 6, 4210752);
-        this.fontRendererObj.drawString(this.playerInventory.getDisplayName().getUnformattedText(), 8, this.ySize - 96 + 2, 4210752);
+        //this.fontRendererObj.drawString(this.playerInventory.getDisplayName().getUnformattedText(), 8, this.ySize - 96 + 2, 4210752);
+        this.fontRendererObj.drawString("Health:", 9, this.ySize - 96 + 3, 4210752);
+        this.fontRendererObj.drawString("Jump:", 65, this.ySize - 96 + 3, 4210752);
+        this.fontRendererObj.drawString("Speed:", 115, this.ySize - 96 + 3, 4210752);
+        drawStats(this.horseEntity.getMaxHealth(), (float)this.horseEntity.getHorseJumpStrength(), (float)this.horseEntity.getEntityAttribute(SharedMonsterAttributes.movementSpeed).getBaseValue());
     }
 
     /**
@@ -77,4 +82,27 @@ public class GuiScreenHorseInventory extends GuiContainer
         this.mousePosY = (float)mouseY;
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
+
+    private void drawStats(float paramFloat1, float paramFloat2, float paramFloat3)
+    {
+        int i = Math.round((paramFloat1 - 15.0F) / 15.0F * 100.0F);
+        int j = Math.round((paramFloat2 - 0.4F) / 0.6F * 100.0F);
+        int k = Math.round((paramFloat3 - 0.1125F) / 0.225F * 100.0F);
+        int m = i < 50 ? (int)((50 - i) / 50.0F * 255.0F) : 0;
+        int n = i > 50 ? (int)((100 - i) / 50.0F * 255.0F) : (int)(i / 50.0F * 255.0F);
+        int i1 = i > 50 ? (int)((i - 50) / 50.0F * 255.0F) : 0;
+        int i2 = m << 16 | i1 << 8 | n;
+        this.fontRendererObj.drawString(Integer.toString(i), 44, this.ySize - 96 + 3, i2);
+        m = j < 50 ? (int)((50 - j) / 50.0F * 255.0F) : 0;
+        n = j > 50 ? (int)((100 - j) / 50.0F * 255.0F) : (int)(j / 50.0F * 255.0F);
+        i1 = j > 50 ? (int)((j - 50) / 50.0F * 255.0F) : 0;
+        i2 = m << 16 | i1 << 8 | n;
+        this.fontRendererObj.drawString(Integer.toString(j), 93, this.ySize - 96 + 3, i2);
+        m = k < 50 ? (int)((50 - k) / 50.0F * 255.0F) : 0;
+        n = k > 50 ? (int)((100 - k) / 50.0F * 255.0F) : (int)(k / 50.0F * 255.0F);
+        i1 = k > 50 ? (int)((k - 50) / 50.0F * 255.0F) : 0;
+        i2 = m << 16 | i1 << 8 | n;
+        this.fontRendererObj.drawString(Integer.toString(k), 149, this.ySize - 96 + 3, i2);
+    }
+
 }
